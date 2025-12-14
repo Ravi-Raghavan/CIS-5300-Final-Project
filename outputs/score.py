7d16c3b45858c749f2586ffc91294e138fda681e
Ravi Raghavan <raviraghavan@Ravis-MacBook-Pro.local>
2025-12-13 21:18:18 -0500

    Changed score function

--- outputs/score.py
+++ outputs/score.py
@@ -30,7 +30,9 @@ y_pred = np.load(args.pred_labels_file).flatten()
 metrics = compute_metrics(y_true, y_pred)
 
 # --- Print required metrics ---
-metric_names = ["Pos F1"]
+metric_names = ["Accuracy", "Pos Precision", "Pos Recall", "Neg Precision", "Neg Recall", 
+                "Pos F1", "Neg F1", "F1 Macro", "F1 Micro", "F1 Weighted"]
 
 for name, value in zip(metric_names, metrics):
-    print(f"{name}: {value:.3f}")
+    if name == "Pos F1":
+        print(f"{name}: {value:.3f}")
